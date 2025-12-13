ae9628885c99eb05a0399ca370f92960c77714bf
Verywell <mael.lebrun0912@gmail.com>
2025-12-13 05:09:26 +0100

    maitre gims

--- push_b.c
+++ push_b.c
@@ -11,25 +11,25 @@ void	push_b(t_stack *a, t_stack *b)
 	int	i;
 	int	size;
 	int	spliter;
+	int	torr;
 
+	torr = 0;
 	spliter = get_spliter(a);
 	size = a->size;
 	i = 0;
 	while (i < size)
 	{
-		if (a->head->inlis)
+		if (a->head->inlis && b->head && b->head->index > spliter)
+			rr(a, b);
+		else if (a->head->inlis)
 			ra(a);
-		else
+		else if (b->head && b->head->index > spliter)
 		{
-			if (a->head->index < spliter)
-				pb(a, b);
-			else
-			{
-				pb(a, b);
-				if (b->size > 1)
-					rb(b);
-			}
+			rb(b);
+			pb(a, b);
 		}
+		else
+			pb(a, b);
 		i++;
 	}
 }
